41e1acfd225c97155440e05635719cd23af7a378
Nigel Lai <nigelelai@gmail.com>
2020-09-22 15:00:29 +0800

    Front page additions

--- app/controllers/translation_requests_controller.rb
+++ app/controllers/translation_requests_controller.rb
@@ -4,5 +4,19 @@ class TranslationRequestsController < ApplicationController
   end
 
   def create
+    @translation_request = TranslationRequest.new(translation_request_params)
+    @translation_request.user = current_user
+    if @translation_request.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def translation_request_params
+    params.require(:translation_request).permit(:original_language, :converted_language, :content)
+  end
+
 end
