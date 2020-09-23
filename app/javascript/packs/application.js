a05585970bb98fb9cda01057030f51bae5b10dfd
miguel jimenez <denkatalabeno@gmail.com>
2020-09-23 19:37:48 +0800

    code is now highlighting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("packs/prism")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +24,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+//import Prism from 'prismjs';
+// import * from 'prismjs/themes/prism.css'
+
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -32,5 +35,6 @@ import conversion from '../components/conversion.js'
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  conversion();
+  //conversion();
+  //Prism.highlightAll();
 });
---
+++ todo.md
@@ -0,0 +1,7 @@
+
+
+TODO PRISM
+----------
+
+[Z] - Can I use this in a yarn/npm package? - YES
+[ ] - Can I apply it 'magically' on text change
