e00f7bfcd1326402cef4022edfad97b57a1a79ea
miguel jimenez <denkatalabeno@gmail.com>
2020-09-23 23:34:20 +0800

    added colorizer and styled card

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,6 +32,20 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import conversion from '../components/conversion.js'
 
+
+
+Prism.plugins.NormalizeWhitespace.setDefaults({
+  'remove-trailing': true,
+  'remove-indent': true,
+  'left-trim': true,
+  'right-trim': true,
+  /*'break-lines': 80,
+  'indent': 2,
+  'remove-initial-line-feed': false,
+  'tabs-to-spaces': 4,
+  'spaces-to-tabs': 4*/
+});
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
