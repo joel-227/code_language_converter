c6fa475838de7fb3bbc876f793c2bea8fc76ef36
miguel jimenez <denkatalabeno@gmail.com>
2020-09-24 15:19:44 +0800

    added translation card with hardcoded logos, images and like

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -50,5 +50,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   //conversion();
-  //Prism.highlightAll();
+  Prism.highlightAll();
 });
