310ae98e8cb7db1fea1818c0832af8f21aaa7c92
Mark Leong <pfleong22@yahoo.com>
2020-09-29 10:16:04 +0800

    Added Kuods points profile

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -11,7 +11,9 @@ class TranslationsController < ApplicationController
     @translation.translation_request = @translation_request
     @translation.language = @translation_request.converted_language
     if @translation.save
-      flash[:notice] = "Your translation has been submitted"
+      @translation.user.kudos_points += @translation.content.split(/\r\n/).count
+      @translation.user.save
+      flash[:notice] = "Thanks for translating, you just won #{@translation.content.split(/\r\n/).count} points"
       redirect_to translation_request_path(@translation_request)
     else
       flash[:notice] = "Sorry, something went wrong"
