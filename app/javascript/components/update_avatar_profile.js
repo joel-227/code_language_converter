994d7745942c9c543e92097be529d07db85452d2
miguel jimenez <denkatalabeno@gmail.com>
2020-10-01 12:02:29 +0800

    added safeguard to activate the js only when in user profile page

--- app/javascript/components/update_avatar_profile.js
+++ app/javascript/components/update_avatar_profile.js
@@ -1,27 +1,24 @@
 
 
 function updateAvatarProfile() {
-  const imagePlaceholder = document.querySelector('.avatar.mb-3');
-  const imageInput = document.querySelector('#user_image_url');
-  imageInput.addEventListener('input', (event) => {
-    // get the value from the input
-    fetch(imageInput.value)
-      .then((res) => {
-        if (res.status === 200) {
-          imagePlaceholder.src = res.url;
-        } else {
-          imagePlaceholder.src = "https://image.flaticon.com/icons/png/512/103/103085.png";
-        }
-      })
-      .catch(error => {
-        console.log("in the error");
-      })
-
-    // fetch to the to the url
-    // if the fetch works
-    // replace the old placeholder
-    // if it doesnt, throw an error
-  })
+  let regex = RegExp("users/edit");
+  if (regex.test(window.location.href)) {
+    const imagePlaceholder = document.querySelector('.avatar.mb-3');
+    const imageInput = document.querySelector('#user_image_url');
+    imageInput.addEventListener('input', (event) => {
+      fetch(imageInput.value)
+        .then((res) => {
+          if (res.status === 200) {
+            imagePlaceholder.src = res.url;
+          } else {
+            imagePlaceholder.src = "https://image.flaticon.com/icons/png/512/103/103085.png";
+          }
+        })
+        .catch(error => {
+          console.log("in the error");
+        })
+    })
+  }
 };
 
 
