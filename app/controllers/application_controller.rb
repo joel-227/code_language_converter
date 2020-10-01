1db5f3fb8321289170d8e502593a6182641a222e
miguel jimenez <denkatalabeno@gmail.com>
2020-10-01 11:55:58 +0800

    profile now updating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:landing, :home]
+
+  private
+
+  def configure_permitted_parameters
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :image_url])
+
+  end
 
 end
