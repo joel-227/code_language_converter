9075ac9ebc64e0472da65dd9cc969d75ed214061
miguel jimenez <denkatalabeno@gmail.com>
2020-10-01 12:28:03 +0800

    edit profile now dynamically adjusts avatar whether there was a previous avatar before or not

--- app/javascript/components/update_avatar_profile.js
+++ app/javascript/components/update_avatar_profile.js
@@ -3,19 +3,25 @@
 function updateAvatarProfile() {
   let regex = RegExp("users/edit");
   if (regex.test(window.location.href)) {
-    const imagePlaceholder = document.querySelector('.avatar.mb-3');
+    const noAvatarPlaceholder = document.querySelector('.no-avatar');
+    const imagePlaceholder = document.querySelector('.avatar-dynamic.avatar.mb-3');
     const imageInput = document.querySelector('#user_image_url');
     imageInput.addEventListener('input', (event) => {
       fetch(imageInput.value)
         .then((res) => {
-          if (res.status === 200) {
+          if (res.status === 200 && imagePlaceholder) {
             imagePlaceholder.src = res.url;
+          } else if (res.status === 200 && noAvatarPlaceholder) {
+            console.log("res is 200");
+            noAvatarPlaceholder.innerHTML = `<img class="avatar-dynamic avatar mb-3" src="${res.url}">`
+          } else if (imagePlaceholder) {
+            imagePlaceholder.src = "https://www.iconfinder.com/data/icons/hosting-glyphs/60/error__attack__dos_404_-512.png";
           } else {
-            imagePlaceholder.src = "https://image.flaticon.com/icons/png/512/103/103085.png";
+            noAvatarPlaceholder.innerHTML =`<img class="avatar mb-3" src="https://www.iconfinder.com/data/icons/hosting-glyphs/60/error__attack__dos_404_-512.png">`;
           }
         })
         .catch(error => {
-          console.log("in the error");
+          console.log(error);
         })
     })
   }
